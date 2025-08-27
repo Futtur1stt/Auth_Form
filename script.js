7874564d13c491597e62c7aa65f9533cbfc08dab
PolyakGG <ipoly04@gmail.com>
2025-08-27 16:08:38 +0300

    Finish work with header. Fix HTML layout, add styles and dark theme on page.

---
+++ script.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+const lightThemeButton = document.querySelector('[data-theme-btn="light"]');
+const darkThemeButton = document.querySelector('[data-theme-btn="dark"]');
+
+const enableDarkTheme = () => {
+  body.classList.add("dark-theme");
+  localStorage.setItem("theme", "dark");
+};
+
+const enableLightTheme = () => {
+  body.classList.remove("dark-theme");
+  localStorage.setItem("theme", "light");
+};
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "dark") {
+  enableDarkTheme();
+} else {
+  enableLightTheme();
+}
+
+darkThemeButton.addEventListener("click", enableDarkTheme);
+lightThemeButton.addEventListener("click", enableLightTheme);
