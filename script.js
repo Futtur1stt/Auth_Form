35f0386bf3f652ebf27d2959f82ce6f2f052a333
PolyakGG <ipoly04@gmail.com>
2025-08-27 22:41:34 +0300

    Fix styles and add a11y, correct tabulation by JS

--- script.js
+++ script.js
@@ -26,11 +26,45 @@ lightThemeButton.addEventListener("click", enableLightTheme);
 const loginButton = document.querySelector("[data-login-btn]");
 const registrationButton = document.querySelector("[data-registration-btn]");
 const registrationBox = document.querySelector(".registration__box");
+const registrationTitleFirst = document.querySelector("[data-title-first]");
+const registrationTitleSecond = document.querySelector("[data-title-second]");
 
 loginButton.addEventListener("click", () => {
   registrationBox.classList.remove("active");
+  setTimeout(() => {
+    registrationTitleFirst.classList.add("visually-hidden");
+    loginButton.classList.add("visually-hidden");
+    registrationTitleSecond.classList.remove("visually-hidden");
+    registrationButton.classList.remove("visually-hidden");
+
+    registrationButton.setAttribute("tabindex", "0");
+    registrationButton.setAttribute("aria-hidden", "true");
+    registrationTitleSecond.setAttribute("aria-hidden", "true");
+
+    loginButton.setAttribute("tabindex", "-1");
+    loginButton.setAttribute("aria-hidden", "false");
+    registrationTitleFirst.setAttribute("aria-hidden", "false");
+
+    registrationButton.focus();
+  }, 300);
 });
 
 registrationButton.addEventListener("click", () => {
   registrationBox.classList.add("active");
+  setTimeout(() => {
+    registrationTitleFirst.classList.remove("visually-hidden");
+    loginButton.classList.remove("visually-hidden");
+    registrationTitleSecond.classList.add("visually-hidden");
+    registrationButton.classList.add("visually-hidden");
+
+    loginButton.setAttribute("tabindex", "0");
+    loginButton.setAttribute("aria-hidden", "true");
+    registrationTitleFirst.setAttribute("aria-hidden", "true");
+
+    registrationButton.setAttribute("tabindex", "-1");
+    registrationButton.setAttribute("aria-hidden", "false");
+    registrationTitleSecond.setAttribute("aria-hidden", "false");
+
+    loginButton.focus();
+  }, 300);
 });
